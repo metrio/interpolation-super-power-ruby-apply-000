4ad5f0a6cd8f5ac9ed7f29682ecad0141b92e2b2
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-17 16:10:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,10 @@
 # Write your #display_rainbow method here
-def display_rainbow 
+def display_rainbow(color)
+  puts #{color[0]}
+  puts #{color[1]}
+  puts #{color[2]}
+  puts #{color[3]}
+  puts #{color[4]}
+  puts #{color[5]}
+  puts #{color[6]}
+end
